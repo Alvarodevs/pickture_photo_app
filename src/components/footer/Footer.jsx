8e746176eaaff53e11ca35a0e3a3f5f4f5f91527
Alvaro Garzón <alvaro.develops@gmail.com>
2022-10-16 23:16:58 +0200

    feat: footer finished

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,49 @@
-import React from 'react'
-import { FooterContainer, Mail, MediaLinks } from './Styled_Footer'
+import React from "react";
+import {
+    FooterContainer,
+    Mail,
+    MediaLinks,
+    MailContainer,
+    Links
+} from "./Styled_Footer";
+import { CopyrightOutlined, ForwardToInboxOutlined, FavoriteOutlined } from "@mui/icons-material";
+import linkedin from "../../assets/images/linkedin.svg";
+import github from "../../assets/images/github.svg";
 
-const Footer = () => 
-{
-  return (
-    <div>
-        <FooterContainer>
-            <Mail>
-                MAIL
-            </Mail>
-            <MediaLinks>
-                MEDIALINLS
-            </MediaLinks>
-        </FooterContainer>
-    </div>
-  )
-}
 
-export default Footer
+const Footer = () => {
+    return (
+        <div>
+            <FooterContainer>
+                <Mail>
+                    <MailContainer>
+                        <CopyrightOutlined />
+                        <span>2022</span>
+                    </MailContainer>
+                    <MailContainer>
+                        <ForwardToInboxOutlined />
+
+                        <a href="mailto:alvaro.develops@gmail.com">
+                            @Alvarodevs
+                        </a>
+                    </MailContainer>
+                </Mail>
+                <MediaLinks>
+                    <span>
+                        Made with <FavoriteOutlined /> by
+                    </span>
+                    <Links>
+                        <a href="https://www.linkedin.com/in/alvarodevs4you/">
+                            <img src={linkedin} alt="Linkedin profile" />
+                        </a>
+                        <a href="https://github.com/Alvarodevs">
+                            <img src={github} alt="Github profile" />
+                        </a>
+                    </Links>
+                </MediaLinks>
+            </FooterContainer>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/footer/Styled_Footer.jsx
+++ src/components/footer/Styled_Footer.jsx
@@ -4,20 +4,69 @@ export const FooterContainer = styled.div`
     position: absolute;
     bottom: 0;
     width: 100%;
-    height: 70px;
+    height: 60px;
     border-top: 1px solid rgba(0, 0, 0, 0.8);
-    box-shadow: 0px -3px 6px rgba(0, 0, 0, 0.4);
+    box-shadow: 0px -3px 6px -3px rgba(0, 0, 0, 0.4);
     display: flex;
     justify-content: space-around;
-    
 `;
 
 export const Mail = styled.div`
-    padding-top: 25px;
     height: 100%;
+    display: flex;
+    align-items: center;
+    @media only screen and (max-width: 520px){
+        display: block;
+        padding-top: 10px;
+    }
+`;
+
+export const MailContainer = styled.div`
+    display: flex;
+    align-items: center;
+    svg {
+        margin-right: 5px;
+    }
+    span,
+    a {
+        font-family: var(--secondary-font);
+        margin-right: 20px;
+        text-decoration: none;
+        color: var(--black);
+    }
 `;
 
 export const MediaLinks = styled.div`
-    padding-top: 25px;
     height: 100%;
+    display: flex;
+    align-items: center;
+
+    svg {
+        color: var(--red);
+        margin: 0 5px;
+    }
+    span {
+        display: flex;
+        align-items: center;
+        margin-right: 10px;
+        font-family: var(--secondary-font);
+    }
+    @media only screen and (max-width: 520px) {
+        display: block;
+    }
+`;
+
+export const Links = styled.div`
+    display: flex;
+    align-items: center;
+    a {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        bottom: 0;
+        font-family: var(--secondary-font);
+        margin-right: 10px;
+        text-decoration: none;
+        color: var(--black);
+    }
 `;
