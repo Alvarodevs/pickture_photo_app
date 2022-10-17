eda3888a4562074eba638dc2b38c422182443a6c
Alvaro Garzón <alvaro.develops@gmail.com>
2022-10-17 12:58:19 +0200

    feat: card shape responsive mobile fixed

--- src/components/cards_grid/Styled_Card_grid.jsx
+++ src/components/cards_grid/Styled_Card_grid.jsx
@@ -6,8 +6,8 @@ export const GridContainer = styled.div`
     padding: 0 20px;
     display: grid;
     grid-auto-rows: 20rem;
-    grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(15rem, 0.7fr));
     gap: 10px;
     overflow: hidden;
-    z-index: 0;
+    justify-content: center;
 `;
